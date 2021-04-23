8a707f48e84e95a5e7fe6653483d052226fca199
Charles CAMPET <44234297+Fr0ggym4n@users.noreply.github.com>
2021-04-23 16:06:05 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const URL = "https://www.omdbapi.com/?apikey=cffce37f&"
+const URL = "https://www.omdbapi.com/?apikey="
 const selector = document.getElementById("movie");
 
 const fetchAll = (movie) => {
